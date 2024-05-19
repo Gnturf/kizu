bd904b8d22f3ce8c1236d062a3c03e6346817855
Nayla Degurechaff <gfirmansyah279@gmail.com>
2024-05-19 11:52:11 +0700

    add connection to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:kizu/firebase_options.dart';
 import 'package:kizu/welcome/screens/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
